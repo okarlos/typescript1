20211d677448d0cae138048d00c4aac019fa0387
Karlos Kliemann <karloskliemann@gmail.com>
2023-06-14 06:04:20 -0300

    parametros opcionais no construtor

--- app/views/view.ts
+++ app/views/view.ts
@@ -1,16 +1,22 @@
 export abstract class View<T> {
 
     protected element: HTMLElement;
+    private escapar = false;
 
-    constructor(selector: string) {
+    constructor(selector: string, escapar?: boolean) {
         this.element = document.querySelector(selector);
+        if (escapar) {
+            this.escapar = escapar;
+        }
     }
 
     protected abstract template(model: T): string;
 
-    update(model: T): void {
-        const template = this.template(model);
-        console.log(template);
+    public update(model: T): void {
+        let template = this.template(model);
+        if (this.escapar) {
+            template = template.replace(/<script>[\s\S]*?<script>/, '')
+        }
         this.element.innerHTML = template;
     }
 }
